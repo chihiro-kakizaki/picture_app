86dee26c0eb9512ebb4af383afa3d6d5ca458767
chihiro-kakizaki <kkzkchr@gmail.com>
2022-02-11 21:09:28 +0900

    アソシエーション設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_11_084013) do
+ActiveRecord::Schema.define(version: 2022_02_11_090259) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pictures", force: :cascade do |t|
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_pictures_on_user_id"
+  end
 
   create_table "pictutres", force: :cascade do |t|
     t.text "content"
@@ -32,5 +40,6 @@ ActiveRecord::Schema.define(version: 2022_02_11_084013) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "pictures", "users"
   add_foreign_key "pictutres", "users"
 end
