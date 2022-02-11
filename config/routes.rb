1d9fa4c4e06c1c435a8d267e71682ff0be375ee5
chihiro-kakizaki <kkzkchr@gmail.com>
2022-02-11 14:30:43 +0900

    add:sessionsのルーティング定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
   get 'pictures/index'
+  resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
 end
 
