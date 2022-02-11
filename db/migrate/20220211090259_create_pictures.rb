e9c0de9505c0faef1965d9f8120b2649884137f4
chihiro-kakizaki <kkzkchr@gmail.com>
2022-02-11 18:57:55 +0900

    add:Picturesテーブル作成

--- db/migrate/20220211090259_create_pictures.rb
+++ db/migrate/20220211090259_create_pictures.rb
@@ -2,7 +2,7 @@ class CreatePictures < ActiveRecord::Migration[6.0]
   def change
     create_table :pictures do |t|
       t.text :content
-      t.references :user, null: false, foreign_key: true
+      t.references :user,  foreign_key: true
 
       t.timestamps
     end
