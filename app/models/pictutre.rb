cf67051e94a04b48832609e47bddb85afec383d9
chihiro-kakizaki <kkzkchr@gmail.com>
2022-02-11 17:43:21 +0900

    add:pictureモデル作

---
+++ app/models/pictutre.rb
@@ -0,0 +1,3 @@
+class Pictutre < ApplicationRecord
+  belongs_to :user
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_11_012104) do
+ActiveRecord::Schema.define(version: 2022_02_11_084013) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pictutres", force: :cascade do |t|
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_pictutres_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -24,4 +32,5 @@ ActiveRecord::Schema.define(version: 2022_02_11_012104) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "pictutres", "users"
 end
