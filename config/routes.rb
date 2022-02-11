6a8fc8194d66d52f37a9a69ac270bfb6b29c3f8e
chihiro-kakizaki <kkzkchr@gmail.com>
2022-02-11 17:49:44 +0900

    add:picturesルーティング定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'pictures/index'
+  resources :pictures
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
 end
