2cb34ce2eead3176d4bb162af74fd5a51f8d053e
chihiro-kakizaki <kkzkchr@gmail.com>
2022-02-15 16:36:21 +0900

    add:favoritesのアソシエーション定義

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -4,6 +4,5 @@ class Picture < ApplicationRecord
   mount_uploader :image, ImageUploader
 
   belongs_to :user
-  has_many :favorites, dependent: :destroy
-   
+  has_many :favorites, dependent: :destroy 
 end
