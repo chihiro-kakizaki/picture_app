f6d12731c1e929b555782b801466a823fbfa3939
chihiro-kakizaki <kkzkchr@gmail.com>
2022-02-16 12:59:20 +0900

    add:favoritesへのルーティング定義

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,12 @@ Rails.application.routes.draw do
     end
   end
   resources :sessions, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create, :show, :edit, :update]
+  resources :users, only: [:new, :create, :show, :edit, :update] 
+  resources :users, only: [:show] do
+    member do
+      get :favorites
+    end
+  end
   resources :favorites, only: [:create, :destroy]
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
