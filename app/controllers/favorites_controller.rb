1ac5d72603d3a22e3c0c35f1ebaa9d19fc6f9c15
chihiro-kakizaki <kkzkchr@gmail.com>
2022-02-16 13:00:49 +0900

    add:create,destroyアクション定義

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -9,6 +9,4 @@ class FavoritesController < ApplicationController
     favorite = current_user.favorites.find_by(id: params[:id]).destroy
     redirect_to pictures_path, notice: "#{favorite.picture.user.name}さんのブログをお気に入り解除しました"
   end
-
-  
 end
