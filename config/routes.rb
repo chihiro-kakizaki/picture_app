78b500f0fbdf0401976befcb641158003f603682
chihiro-kakizaki <kkzkchr@gmail.com>
2022-02-17 10:46:43 +0900

    fix:letter_openerを本番環境で使えるように設定

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,6 @@ Rails.application.routes.draw do
     end
   end
   resources :favorites, only: [:create, :destroy]
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" 
 end
 
